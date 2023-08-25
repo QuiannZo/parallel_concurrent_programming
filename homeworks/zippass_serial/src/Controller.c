0fae95e2eaf6ff6b04d9c27099b3500e082c61cb
QuiannZo <quiannzolfa23@gmail.com>
2023-08-25 10:09:24 -0600

    NOTE: This commit contains previous work not done in the right repo. As advances are not significant this commit will sumarize the first achievements done on this program, which basicly consist on opening a file given by the user and getting the data.
    On branch main
    Changes to be committed:
    modified:   homeworks/zippass_serial/.gitignore
        Added a gitignore with the hw specifications.
    new file:   homeworks/zippass_serial/Makefile
        Added the professor makefile and added the ziplib library
        flag for compilation
    new file:   homeworks/zippass_serial/README.md
        Initial commit README. Not modified yet
    new file:   homeworks/zippass_serial/src/Controller.c
        Controller for the application. At the moment contains the
        implementation for three functions.
    new file:   homeworks/zippass_serial/src/Controller.h
        header file for Controller.c
    new file:   homeworks/zippass_serial/src/main.c
        Added libzip header.
        created a file manager for testing.
        The code gets and stores the characters and zip dir.
    new file:   homeworks/zippass_serial/tests/
        Added the tests folder given by the professor.

---
+++ homeworks/zippass_serial/src/Controller.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <zip.h>
+
+#include "Controller.h"
+
+void readData(FILE* args, char chars[]){
+    
+}
+
+void findPass(){
+
+}
+
+void printData(){
+
+}
---
+++ homeworks/zippass_serial/src/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <zip.h>
+
+#include "Controller.h"
+
+int main(int argc, char* argv[]){
+    int maxLen; // Maximum password length.
+    char chars[100]; // chars by the input.
+    char paths[20][256]; // paths of the zip files.
+
+    char* filename;
+    if(argc > 1 && argc <= 2){
+        // If the file is a text file:
+        if (strcmp(argv[1], "-f") == 0){
+            filename = argv[2];
+            FILE* argument = fopen(filename, "r");
+            if (argument == NULL) {
+                printf("Error: Could not open file '%s'\n", filename);
+                return EXIT_FAILURE; // returns 1.
+            }
+
+            int c = 0;
+            int character = 0;
+            while((character = getc(argument)) != EOF && character != '\n'){
+                chars[c] = character;
+                c++;
+            }
+
+            // get the max length.
+            fscanf(argument, "%d\n\n", &maxLen);
+
+            // get the file paths.
+            int str_counter = 0;
+            while (fgets(paths[str_counter], sizeof(paths[str_counter]), argument) != NULL){
+                // Quit the '\n' at the end of the lines to avoid errors.
+                // This piece of code was gathered from chatgpt 3.5
+                int len = strlen(paths[str_counter]);
+                if (len > 0 && paths[str_counter][len - 1] == '\n') {
+                    paths[str_counter][len - 1] = '\0'; // Replace '\n' with '\0'
+                }
+                str_counter++;
+            }
+
+            // close the file.
+            fclose(argument);
+        }
+    }
+
+    printf("%c\n" ,chars[0]);
+    printf("%d\n" ,maxLen);
+    printf("%s\n" ,paths[0]);
+    printf("%s\n" ,paths[1]);
+}
