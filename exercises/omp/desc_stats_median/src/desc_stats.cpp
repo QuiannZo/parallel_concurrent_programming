9be3aa7b12aa0c1f58fea798edba4049adb9dd52
Quiann Zolfaghari <quiann.zolfaghari@ucr.ac.cr>
2023-10-31 04:27:12 -0600

    Classwork. desc median.

---
+++ exercises/omp/desc_stats_median/src/desc_stats.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <omp.h>
+#include <algorithm>
+
+int main() {
+    std::vector<double> numbers;
+    double num;
+    
+    // Leer números flotantes de la entrada estándar.
+    while (std::cin >> num) {
+        numbers.push_back(num);
+    }
+
+    // Asegurarse de que haya al menos un número.
+    if (numbers.empty()) {
+        std::cerr << "No se ingresaron números." << std::endl;
+        return 1;
+    }
+
+    // Calcular los datos concurrentemente utilizando omp.
+    double min_val = std::numeric_limits<double>::max();
+    double max_val = std::numeric_limits<double>::min();
+    double sum = 0.0;
+    double sum_squared = 0.0;
+
+    #pragma omp parallel for reduction(min:min_val) reduction(max:max_val) reduction(+:sum) reduction(+:sum_squared)
+    for (size_t i = 0; i < numbers.size(); i++) {
+        double num = numbers[i];
+        min_val = std::min(min_val, num);
+        max_val = std::max(max_val, num);
+        sum += num;
+        sum_squared += num * num;
+    }
+
+    // promedio y la desviación estándar.
+    double average = sum / numbers.size();
+    double variance = (sum_squared / numbers.size()) - (average * average);
+    double std_deviation = std::sqrt(variance);
+
+    // Ordenar los números para calcular la mediana
+    std::sort(numbers.begin(), numbers.end());
+
+    // Calcular la mediana
+    double median;
+    size_t n = numbers.size();
+    if (n % 2 == 0) {
+        median = (numbers[n / 2 - 1] + numbers[n / 2]) / 2.0;
+    } else {
+        median = numbers[n / 2];
+    }
+
+    std::cout << "Mínimo: " << min_val << std::endl;
+    std::cout << "Promedio: " << average << std::endl;
+    std::cout << "Desviación Estándar: " << std_deviation << std::endl;
+    std::cout << "Máximo: " << max_val << std::endl;
+    std::cout << "Mediana: " << median << std::endl;
+
+    return 0;
+}
